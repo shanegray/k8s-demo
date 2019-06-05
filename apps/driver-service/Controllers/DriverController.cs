5c35166f2ba98fbc29bb9790e2e2d8f662264e15
Shane Gray <shane.joseph.gray@gmail.com>
2019-06-05 16:32:49 +0100

    copy over skeleton

---
+++ apps/driver-service/Controllers/DriverController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DriverService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok(new string[] { "one", "two" });
+        }
+    }
+}
---
+++ apps/package-service/Controllers/PackageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PackageService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PackageController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok(new string[] { "one", "two" });
+        }
+    }
+}
